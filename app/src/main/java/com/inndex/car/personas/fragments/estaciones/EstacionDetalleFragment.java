853d78093fc5d05f9763891860ef1a4416a3f86d
alexander8651 <diego8651@hotmail.com>
2021-01-31 14:42:34 -0500

    termino boton llamar

--- app/src/main/java/com/inndex/car/personas/fragments/estaciones/EstacionDetalleFragment.java
+++ app/src/main/java/com/inndex/car/personas/fragments/estaciones/EstacionDetalleFragment.java
@@ -1,15 +1,19 @@
 package com.inndex.car.personas.fragments.estaciones;
 
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
@@ -252,6 +256,17 @@ public class EstacionDetalleFragment extends Fragment {
                         linearLayout.setVisibility(View.VISIBLE);
                         final TextView numero = root.findViewById(R.id.numero_estacion);
                         numero.setText(estaciones.getTelefono());
+
+                        final CardView botonLlamar = root.findViewById(R.id.botonllamar);
+
+                        botonLlamar.setOnClickListener(view ->{
+                            Intent i = new Intent(Intent.ACTION_CALL);
+                            i.setData(Uri.parse(estaciones.getTelefono()));
+                            startActivity(i);
+                        });
+
+
+
                     }else {
                         params.height = 900;
 
